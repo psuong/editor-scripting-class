f98451cd00aeb5765e1410545bc17407c984d36d
Porrith Suong <psuong95@gmail.com>
2017-05-07 22:33:45 -0400

    Adds the final part of the editor scripting class.

--- Editor-Scripting-Class/Assets/Editor/SampleClassEditor.cs
+++ Editor-Scripting-Class/Assets/Editor/SampleClassEditor.cs
@@ -12,12 +12,21 @@ public class SampleClassEditor : Editor {
 
     // Use the OnEnable() function as your constructor or Start() function in Unity.
     private void OnEnable() {
+        // Find the script we're writing a custom inspector for
         enemyInfoProperty = serializedObject.FindProperty("enemyInfo");
+
+        // Create the reordable list
+        // It should target the "serializedObject" and some serializedProperty like enemyInfoProperty
+        enemyInfoList = new ReorderableList(serializedObject, enemyInfoProperty, true, true, true, true);
+
+        // We're going to use delegates and lambdas here!
+        enemyInfoList.drawHeaderCallback += (Rect rect) => { EditorGUI.LabelField(rect, new GUIContent("Enemy Info List")); };
+        enemyInfoList.drawElementCallback += OnDrawElement;
     }
 
     // Use OnDisable() as a destructor
     private void OnDisable() {
-        
+        enemyInfoList.drawElementCallback -= OnDrawElement;
     }
 
     public override void OnInspectorGUI() {
@@ -26,12 +35,21 @@ public class SampleClassEditor : Editor {
 
         EditorGUI.BeginChangeCheck(); // Start checking whether or not the Editor has been changed
 
-        // Our list will go here.
+        // Our list will go here
+        enemyInfoList.DoLayoutList();
         
         // If the inspector's values change...
         if (EditorGUI.EndChangeCheck()) {
             // Apply the changes with undo support!
             serializedObject.ApplyModifiedProperties(); 
         }
+    }
+
+    private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused) {
+        SerializedProperty element = enemyInfoProperty.GetArrayElementAtIndex(index); // Grab the serialized element via the index
+
+        // Use the FindPropertyRelative to get relative properties, or properties within classes and structs
+        EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width / 2, rect.height), element.FindPropertyRelative("name"), GUIContent.none);
+        EditorGUI.PropertyField(new Rect(rect.x + rect.width / 2, rect.y, rect.width/2, rect.height), element.FindPropertyRelative("state"), GUIContent.none);
     }
 }
