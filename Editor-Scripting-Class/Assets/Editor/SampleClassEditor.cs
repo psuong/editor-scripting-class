d0234fce2e30fe6df3695b93db5020ea9a0c2db6
Porrith Suong <psuong95@gmail.com>
2017-05-07 21:02:25 -0400

    Updates the gitignore.

---
+++ Editor-Scripting-Class/Assets/Editor/SampleClassEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; // You must use this namespace to do Editor scripting - otherwise you will be using UnityEditor.Editor.
+using UnityEditorInternal; // No man's land, there isn't any documentation for this publicly from Unity.
+
+[CustomEditor(typeof(SampleClass))] // Another decorator - but this one extends functionality to target a particular class.
+public class SampleClassEditor : Editor {
+
+    private SerializedProperty enemyInfoProperty;
+    private ReorderableList enemyInfoList;
+
+    // Use the OnEnable() function as your constructor or Start() function in Unity.
+    private void OnEnable() {
+        enemyInfoProperty = serializedObject.FindProperty("enemyInfo");
+    }
+
+    // Use OnDisable() as a destructor
+    private void OnDisable() {
+        
+    }
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+    }
+}
---
+++ Editor-Scripting-Class/Assets/Scripts/SampleClass.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum State { Ally, Enemy, Neutral, Prop }
+
+[System.Serializable]
+// Our struct is a MonoBehaviour, so we must serialize the struct to show in
+// the inspector.
+public struct EnemyInfo {
+    public string name;
+    public State state;
+}
+
+
+public class SampleClass : MonoBehaviour {
+
+    // Non decorated
+    public float uSlider = 120;
+    public string uComment;
+
+    // Decorators
+    // The following decoratored code provides some neat and good spacing for our Inspector
+    [Header("Decorators")]
+    [Range(0f, 360f)]
+    public float slider = 120;
+    [Tooltip("Showing some Unity basic decorators")]
+    [TextArea]
+    public string comment;
+    
+    [Space]
+    [HideInInspector] // We're going to hide our enemyInfo array for when we do editor scripting.
+    public EnemyInfo[] enemyInfo;
+}
